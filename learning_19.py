f7820e307dd29c5ac7d35e67c2d733b69b3e2b44
Adarsh-Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-05-09 23:12:15 +0530

    Add files via upload

---
+++ learning_19.py
@@ -0,0 +1,26 @@
+#dictionaries
+#can also be used with integers
+
+
+
+color = {"b":"blue","o":"orange","g":"green"}
+
+print(color.get("k", "stop"))
+print(color.get("b"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
