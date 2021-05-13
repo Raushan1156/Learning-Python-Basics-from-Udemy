6df4f88230d7b2e52d5d6f41de108be06e1ac10a
Adarsh-Saurabh <75805225+Adarsh-Saurabh@users.noreply.github.com>
2021-05-14 02:11:54 +0530

    Update finding_pi.py

--- Random Practice/finding_pi.py
+++ Random Practice/finding_pi.py
@@ -1,3 +1,8 @@
+#Finding the value of pi using series LEIBNIZ (1-1/3+1/5-1/7..............)
+
+#By calculating it to the value of 10^8. I was able to get the accuracy up to 6 decimal places
+
+
 x = 0
 for i in range(0,int(input())):
   
